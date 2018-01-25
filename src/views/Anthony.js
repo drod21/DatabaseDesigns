faf0aea167f67581ae97967407d14f520db1b872
anthonytcollege <35807929+anthonytcollege@users.noreply.github.com>
2018-01-25 13:08:07 -0500

    add Anthony Component

---
+++ src/views/Anthony.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Anthony extends Component
+{
+    render() {
+        return(
+            <h3>I am Anthony</h3>
+
+        )
+    }
+}
+
+
+export default Anthony
