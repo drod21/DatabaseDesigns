6725a69c940e82537df5feb06ed8d9c58e12ea50
Derek Rodriguez <drod2169@gmail.com>
2018-02-01 10:16:09 -0500

    Add redirect to anthony component to App.js

--- src/views/App.js
+++ src/views/App.js
@@ -15,6 +15,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Link to='about'><button>Test React Router</button></Link>
+        <Link to='Anthony'>Go to Anthony component</Link>
         <br />
         <br />
         <button onClick={this.props.actions.expressTest}>Test if Express is working</button>
