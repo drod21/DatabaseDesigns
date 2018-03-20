dda3e569911c961278e62cf6c541dc18b9163c05
Derek Rodriguez <drod2169@gmail.com>
2018-03-20 08:15:34 -0400

    Seed with dummy data

--- db/seed.js
+++ db/seed.js
@@ -1,51 +1,54 @@
 const db = require('../db')
+const crypto = require('crypto')
 
-const seedProducts = () => db.Promise.map([
-  {title: 'Hats',category: ['Clothes','Accessories'], current_price: 16, description: 'Fedora with a feather', availability: true, inventory: 100, promo_id: 1},
-  {title: 'Ski Suits',category: ['Athletics', 'Clothes'], current_price: 11, description: 'Full body ski suit', availability: false, inventory: 5, promo_id: 1},
-  {title: 'Fanny Pack',category: ['Accessories'], current_price: 12, description: 'Bright neon in all colors', availability: true, inventory: 64, promo_id: 1},
-  {title: 'Chuck Taylors',category: ['Clothes','Shoes'], current_price: 15, description: 'A variation on a classsic', availability: false, inventory: 35},
-  {title: 'Hairspray',category: ['Beauty'], current_price: 41, description: 'Full of CFCs', availability: true, inventory: 22, promo_id: 2},
-  {title: 'Socks',category: ['Clothes'], current_price: 51, description: 'Big wooly socks', availability: true, inventory: 21},
-  {title: 'Wigs', category: ['Accessories', 'Beauty'], current_price: 21, description: 'Business in the front, party in the back', availability: false, inventory: 100, promo_id: 2},
-  {title: 'Chanel', category: ['Beauty'], current_price: 49, description: 'Chanel loose powder', availability: true, inventory: 100, promo_id: 2},
-  {title: 'Cosmetics', category: ['Beauty'], current_price: 31, description: 'Revlon violet pink set', availability: true, inventory: 100, promo_id: 2},
-  {title: 'Covergirl', category: ['Beauty'], current_price: 21, description: 'Cosmetics for the young generation', availability: false, inventory: 100, promo_id: 2},
-  {title: 'Dior', category: ['Beauty'], current_price: 49, description: 'Luxury for your face', availability: true, inventory: 100, promo_id: 2},
-  {title: 'Dolls', category: ['Accessories'], current_price: 9, description: 'For your room', availability: false, inventory: 100, promo_id: 2},
-  {title: 'HighHeels', category: ['Shoes'], current_price: 27, description: 'Edge on your heel', availability: true, inventory: 100, promo_id: 2},
-  {title: 'PinkSuit', category: ['Accessories', 'Clothes'], current_price: 22, description: 'Everything for your pink day', availability: false, inventory: 100, promo_id: 2},
-  {title: 'Technicolor', category: ['Accessories', 'Clothes'], current_price: 31, description: 'Freedom', availability: false, inventory: 100, promo_id: 2},
-  {title: 'Sneakers', category: ['Shoes', 'Athletics'], current_price: 62, description: 'Comply with your style', availability: false, inventory: 100, promo_id: 2},
-  {title: 'FlyWatch', category: ['Accessories'], current_price: 20, description: 'What time is it now? 19: 08 ! ', availability: false, inventory: 100, promo_id: 2},
-  {title: 'ThatWig', category: ['Accessories', 'Beauty'], current_price: 21, description: 'Party people', availability: false, inventory: 100, promo_id: 1},
-  {title: 'Training set', category: ['Athletics', 'Clothes'], current_price: 21, description: 'Lion look', availability: false, inventory: 100, promo_id: 1},
-  {title: 'Training suit for couple', category: ['Athletics', 'Clothes'], current_price: 21, description: 'Lion look', availability: false, inventory: 100, promo_id: 1},
-], product => db.model('products').create(product));
+const hash = crypto.createHash('md5').update('Apples12').digest('hex');
 
-const seedReviews = () => db.Promise.map([
- {rating: 1, review_text: "awful",product_id:5},
- {rating: 1, review_text: "if you have too much extra money ",product_id:1},
- {rating: 5, review_text: "the best!",product_id:2},
- {rating: 2, review_text: "waste of money",product_id:3},
- {rating: 3, review_text: "can be better",product_id:4},
- {rating: 3, review_text: "should be better",product_id:6},
- {rating: 4, review_text: "good price",product_id:7},
- {rating: 4, review_text: "just like description",product_id:1}
- ], review => db.model('reviews').create(review));
+const seedItems = () => db.Promise.map([
+  { id: getRandomInt(42), item_name: 'Hats', dept_id: 26, type: 'Clothes', description: 'Fedora with a feather', price_public: 10.4, price_private: 9.2},
+  { id: getRandomInt(23), item_name: 'Samsung TV 52 inch OLED 4k', dept_id: 23, type: 'Entertainment', description: '52" Samsung OLED TV! 4k resolution! 240Hz refresh rate!', price_public: 699.99, price_private: 574.99 },
+  { id: getRandomInt(234), item_name: 'Nintendo Switch', dept_id: 21, type: 'Entertainment', description: 'Nintendo Switch', price_public: 299.99, price_private: 249.99 },
+  { id: getRandomInt(421), item_name: 'Star Wars', dept_id: 25, type: 'Movie', description: 'Episode 4: A New Hope', price_public: 9.99, price_private: 4.99 },
+], item => db.model('Items').create(item));
+
+const seedEmployees = () => db.Promise.map([
+  { eid: 21, emp_name: "Derek Rodriguez", email: 'derek23@mail.usf.edu', emp_pw: hash, dept_id: 23, manager_id: 1 },
+  { eid: 25, emp_name: "Anthony Teresi", email: 'anthony@gmail.com', emp_pw: hash, dept_id: 21, manager_id: 4 },
+  { eid: 32, emp_name: "Joe Schmoe", email: 'joe@gmail.com', emp_pw: hash, dept_id: 26, manager_id: 3 },
+  { eid: 35, emp_name: 'Brianna Grace', email: 'brianna@gmail.com', emp_pw: hash, dept_id: 25, manager_id: 2},
+  { eid: 1, emp_name: "Josh Circuit", email: 'josh@gmail.com', emp_pw: hash, dept_id: 23, manager_id: 0 },
+  { eid: 4, emp_name: "Robert Albrechtsson", email:'robert@gmail.com', emp_pw: hash, dept_id: 21, manager_id: 0 },
+  { eid: 3, emp_name: "Cola MacCrumb", email: 'cola@gmail.com', emp_pw: hash, dept_id: 26, manager_id: 0 },
+  { eid: 2, emp_name: 'Steven Spielberg', email: 'steven@gmail.com', emp_pw: hash, dept_id: 25, manager_id: 0 }
+
+], employee => db.model('Employees').create(employee));
+
+ const seedManagers = () => db.Promise.map([
+   { mid: 1, dept_id: 23 },
+   { mid: 3, dept_id: 26 },
+   { mid: 4, dept_id: 21 },
+   { mid: 2, dept_id: 25 }
+ ], (manager) => db.model('Managers').create(manager));
 
  const seedDepartments = () => db.Promise.map([
    { dept_id: 23, name: 'Electronics' },
    { dept_id: 26, name: 'Home Goods' },
    { dept_id: 21, name: 'Games' },
    { dept_id: 25, name: 'Movies' },
- ], department => db.model('departments').create(department));
+ ], department => db.model('Departments').create(department));
  
  db.didSync
    .then(() => db.sync({force: true}))
-   .then(seedProducts)
-   .then(products => console.log(`Seeded ${products.length} products OK`))
-   .then(seedReviews)
-   .then(reviews => console.log(`Seeded ${reviews.length} reviews OK`))
+   .then(seedItems)
+   .then(items => console.log(`Seeded ${items.length} items OK`))
+   .then(seedEmployees)
+   .then(emps => console.log(`Seeded ${emps.length} employees OK`))
+   .then(seedManagers)
+   .then(mgrs => console.log(`Seeded ${mgrs.length} managers OK`))
+   .then(seedDepartments)
+   .then(dpts => console.log(`Seeded ${dpts.length} departments OK`))
    .catch(error => console.error(error))
    .finally(() => db.close())
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
