40bb4262944a6c1ea6274db46e1f7fcb5204490d
Derek Rodriguez <drod2169@gmail.com>
2018-04-11 06:00:12 -0400

    Add roles for login **BACKEND ONLY **

--- server/login.js
+++ server/login.js
@@ -11,8 +11,18 @@ router.put('/login', function (req, res, next) {
     where: { email: req.body.email },
   }).then((result) => {
     const hash = crypto.createHash('md5').update(req.body.password).digest('hex');
+    const emp_id = result.emp_id
+    
+    if(emp_id === 0) {
+      role = 'CEO'
+    } else if(emp_id >= 1 && emp_id <= 4) {
+      role = 'manager'
+    } else {
+      role = 'employee'
+    }
+
     const token = Object.assign({}, {
-      token: jwt.sign({ emp_id: result.emp_id, role: 'employee' }, KEY, { expiresIn: '5 days' })
+      token: jwt.sign({ emp_id: emp_id, role: role }, KEY, { expiresIn: '5 days' })
     })
     console.log(token.token)
     if(hash == result.emp_pw)
